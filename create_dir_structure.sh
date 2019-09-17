ff3e4f3a0502b2e15d4fd67b9646b7672073350d
baldmer <baldmer.w@gmail.com>
2019-09-17 15:29:35 +0200

    create init dir structure

--- create_dir_structure.sh
+++ create_dir_structure.sh
@@ -9,7 +9,28 @@ transe_dir="datasets/transe_dir"
 wikidata_dir="datasets/wikidata_dir"
 [ ! -d "$wikidata_dir" ] && mkdir -p "$wikidata_dir"
 
+embed_dir="datasets/embed_dir"
+[ ! -d "$embed_dir" ] && mkdir -p "$embed_dir"
 
-#no_oov_handling_base_mem  oov_handling_matching  preprocessed_data_full_cqa  transe_dir
-#no_oov_handling_new_mem   oov_handling_pca       simple_cqa_dataset          wikidata_dir
+vocabs_dir="vocabs"
+[ ! -d "$vocabs_dir" ] && mkdir -p "$vocabs_dir"
 
+# where final pre-processed dataset files are created
+
+newdir="datasets/no_oov_handling_base_mem"
+[ ! -d "$newdir" ] && mkdir -p "$newdir"
+
+newdir="datasets/no_oov_handling_new_mem"
+[ ! -d "$newdir" ] && mkdir -p "$newdir"
+
+newdir="datasets/oov_handling_matching"
+[ ! -d "$newdir" ] && mkdir -p "$newdir"
+
+newdir="datasets/oov_handling_pca"
+[ ! -d "$newdir" ] && mkdir -p "$newdir"
+
+newdir="datasets/oov_handling_pca"
+[ ! -d "$newdir" ] && mkdir -p "$newdir"
+
+# extracted simple cqa datasets are created by extraction script.
+ 
